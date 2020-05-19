71e45f90dffb7b63c69ba3ad124b02ce5618a86a
钟森 <zhongsen@evercreative.com.cn>
2020-05-19 10:00:07 +0800

    first commit

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * 测试类
+ * @author : Mr-Z
+ * @date : 2020/05/19 09:52
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
